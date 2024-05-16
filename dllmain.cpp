2f485999fd4a4fe62d963e587a9c2b6fe3872297
DAN™ <dranger003@gmail.com>
2024-05-16 06:55:41 -0400

    Update 1.11.36.0.

--- dllmain.cpp
+++ dllmain.cpp
@@ -67,12 +67,12 @@ struct hkConsoleOutputLine
 			jmp(ptr[rip + label1]);
 
 			L(label1);
-			dq(RelocAddr<decltype(&Hook)>(0x0286F005 + 5).getUIntPtr()); // 0x0286ED55
+			dq(RelocAddr<decltype(&Hook)>(0x02917625 + 5).getUIntPtr()); // 0x0286F005
 		}
 	};
 
 	static void Apply() {
-		g_Trampoline.write5Branch(RelocAddr<uintptr_t>(0x0286F005).getUIntPtr(), (uintptr_t)&Hook); // 0x0286ED55
+		g_Trampoline.write5Branch(RelocAddr<uintptr_t>(0x02917625).getUIntPtr(), (uintptr_t)&Hook); // 0x0286F005
 	}
 
 	static void Hook(void* unk1, void* unk2) {
@@ -99,7 +99,7 @@ static void SFSEPlugin_EventCallback(SFSEMessagingInterface::Message* msg)
 extern "C"
 {
 	__declspec(dllexport) SFSEPluginVersionData SFSEPlugin_Version = {
-		SFSEPluginVersionData::kVersion, 1, "Console Hooks", "stonegdi", 0, 0, { RUNTIME_VERSION_1_10_32, 0 }, 0, 0, 0,
+		SFSEPluginVersionData::kVersion, 1, "Console Hooks", "stonegdi", 0, 0, { RUNTIME_VERSION_1_11_36, 0 }, 0, 0, 0,
 	};
 
 	__declspec(dllexport) bool SFSEPlugin_Preload(const SFSEInterface* sfse) {
